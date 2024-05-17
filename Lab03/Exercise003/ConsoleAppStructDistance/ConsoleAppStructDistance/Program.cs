78df8ec28de6fde599783540392c2e109e093d52
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-17 14:16:35 +0300

    first vision
    - вывод на экран 15' 8"

---
+++ Lab03/Exercise003/ConsoleAppStructDistance/ConsoleAppStructDistance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace ConsoleAppStructDistance
+{
+    public struct Distance
+    {
+        public uint foot;               //отрицательным быть не может
+        public uint inch;               //отрицательным быть не может
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Distance d1;
+            
+            //--.
+            d1.foot = 15;
+            d1.inch = 8;
+
+            //--.            
+            Console.WriteLine($"Итого получилось: {d1.foot}'- {d1.inch}\" ") ;
+        }
+    }
+}
