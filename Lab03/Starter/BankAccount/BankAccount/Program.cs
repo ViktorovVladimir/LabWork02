8f45ed2d6b2e3d00390908e036f7b11b76a5a921
ViktorovVladimir <167399035+ViktorovVladimir@users.noreply.github.com>
2024-05-17 11:42:47 +0300

    cosmetic change
    - поменял расположения репозитория на диске, собрал проект

--- Lab03/Starter/BankAccount/BankAccount/Program.cs
+++ Lab03/Starter/BankAccount/BankAccount/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+
+// See https://aka.ms/new-console-template for more information
+
+
+
+
 Console.WriteLine("Hello, World!");
--- Lab03/Starter/BankAccount/BankAccount/obj/Debug/net8.0/BankAccount.AssemblyInfo.cs
+++ Lab03/Starter/BankAccount/BankAccount/obj/Debug/net8.0/BankAccount.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323dd3a56039565e56938074a449c91dd9b6ee7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+969bb0fa9a225ef80f42cdceeb3e37cc91843e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankAccount")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
